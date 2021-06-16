c6cd1c483671ed96dac0818f8ef00fc83c9a285f
www42 <thomas.jaekel@gfn.training>
2021-06-16 09:09:11 +0200

    Change destination to user student

--- scripts/script32.ps1
+++ scripts/script32.ps1
@@ -2,9 +2,9 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name az -Force
 
 $source1 = 'https://raw.githubusercontent.com/www42/AZ-303-Microsoft-Azure-Architect-Technologies/master/tj/demo-managed-ID/ARM.ps1'
-$destination1 =  "$HOME\Desktop\ARM.ps1"
+$destination1 =  "C:\Users\Student\Desktop\ARM.ps1"
 Invoke-WebRequest -Uri $source1 -OutFile $destination1
 
 $source2 = 'https://raw.githubusercontent.com/www42/AZ-303-Microsoft-Azure-Architect-Technologies/master/tj/demo-managed-ID/PowerShell.ps1'
-$destination2 =  "$HOME\Desktop\PowerShell.ps1"
+$destination2 =  "C:\Users\Student\PowerShell.ps1"
 Invoke-WebRequest -Uri $source2 -OutFile $destination2
